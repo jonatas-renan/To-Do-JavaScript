54df47d5363f70f0c7d47206c7e917b655f16ac1
Jônatas Renan <jr.cesar2213@gmail.com>
2026-06-02 15:42:37 -0300

    feat: Adição de filtros para as tarefas

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,117 @@
-// função que adiciona tarefa
-function addTask() {
+// Acessando os elementos do DOM (Botões)
+const addBtn = document.querySelector('#add-btn');
+const filterBtn = document.querySelectorAll('.filter-btn');
 
-    //titulo da tarefa
+// Cria e insere uma nova tarefa na lista com base no input do usuário
+
+function addTask() {
+    
     const taskTitle = document.querySelector('#task-title').value;
 
-    if(taskTitle){
+    if (taskTitle) {
 
-        // clona template
         const template = document.querySelector('.template');
-
         const newTask = template.cloneNode(true);
 
-        // adiciona titulo
         newTask.querySelector('.task-title').textContent = taskTitle;
+        newTask.classList.remove('template', 'hide');
 
-        // remover as classes desnecessarias
-        newTask.classList.remove('template');
-        newTask.classList.remove('hide');
-
-        // adiciona tarefa na lista
         const list = document.querySelector('#task-list');
-
         list.appendChild(newTask);
 
-        // adiicionar o evento de remover
-        const removeBtn = newTask.querySelector('.remove-btn').addEventListener('click', function(){
+        // Atribuição de eventos diretamente nos elementos dinâmicos criados
+        newTask.querySelector('.remove-btn').addEventListener('click', function() {
             removeTask(this);
         });
 
-        // adicionar evento de completar tarefa
-        const doneBtn = newTask.querySelector('.done-btn').addEventListener("click", function(){
+        newTask.querySelector('.done-btn').addEventListener("click", function() {
             completeTask(this);
         });
 
-        // limpar texto
+        // limpa texto do input
         document.querySelector('#task-title').value = "";
-
     }
-
 }
 
-// função de remover tarefa
-function removeTask(task){
+// Remove a tarefa do DOM a partir do elemento filho acionado
+function removeTask(task) {
     task.parentNode.remove(true);
 }
 
-// função de completar a tarefa
-function completeTask(task){
+// Alterna o estado de conclusão da tarefa adicionando/removendo a classe de estilo
+function completeTask(task) {
     const taskToComplete = task.parentNode;
-
     taskToComplete.classList.toggle('done');
 }
 
+// Exibe todas as tarefas ativas e concluídas da lista.
+function filterTaskAll(btn) {
 
-// evento de adicionar tarefa
-const addBtn = document.querySelector('#add-btn');
+    document.querySelector('.filter-btn.active').classList.remove('active');
+    btn.classList.add('active');
 
-addBtn.addEventListener("click", function(e){
+    const list = document.querySelectorAll('.task-box:not(.template)');
 
+    list.forEach(li => {
+
+        li.classList.remove('hide');
+
+    });
+}
+
+// Filtra a lista para exibir apenas as tarefas que não possuem marcação de concluídas.
+function filterTaskActives(btn) {
+
+    document.querySelector('.filter-btn.active').classList.remove('active');
+    btn.classList.add('active');
+
+    const list = document.querySelectorAll('.task-box:not(.template)');
+
+    list.forEach(li => {
+
+        if (li.classList.contains('done')) {
+            li.classList.add('hide');
+        } else {
+            li.classList.remove('hide');
+        }
+
+    });
+}
+
+// Filtra a lista para exibir apenas as tarefas marcadas como concluídas.
+function filterTaskCompleted(btn) {
+
+    document.querySelector('.filter-btn.active').classList.remove('active');
+    btn.classList.add('active');
+
+    const list = document.querySelectorAll('.task-box:not(.template)');
+    
+    list.forEach(li => {
+
+        if (!li.classList.contains('done')) {
+            li.classList.add('hide');
+        } else {
+            li.classList.remove('hide');
+        }
+
+    });
+}
+
+// Evento para envio do formulário e criação de tarefa
+addBtn.addEventListener("click", function(e) {
     e.preventDefault();
-
     addTask();
+});
 
+// Eventos de clique para os seletores de filtros
+filterBtn[0].addEventListener('click', function(e) {
+    filterTaskAll(this);
+});
+
+filterBtn[1].addEventListener('click', function(e) {
+    filterTaskActives(this);
+});
+
+filterBtn[2].addEventListener('click', function(e) {
+    filterTaskCompleted(this);
 });
